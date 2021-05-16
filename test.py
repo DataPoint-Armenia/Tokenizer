1e50618c37afcb154474afcf25889b3a25c9278d
sourenp <souren@papazian.info>
2021-05-16 17:35:51 +0400

    Rename class from Tokenizer to VortanTokenizer

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #import Tokenizer
-from tokenizer import Tokenizer
+from tokenizer import VortanTokenizer
 
-#write some text
+# write some text
 text = 'Նա սենյակ մտավ՝ ծածկելու պատուհանները, երբ մենք դեռ անկողնում էինք, ու տեսա, որ տեսքը հիվանդի է 5-ական 5կմ/ժ -5 հեյ~ ՀՀԿ-ական սենյակ։'
 
 long_text = """
@@ -12,9 +12,9 @@ long_text = """
 — Գլո՛ւխս է ցավում։
 """
 
-#tokenization
-T = Tokenizer(long_text)
+# tokenization
+T = VortanTokenizer(long_text)
 T.segmentation().tokenization()
 
-#print result
+# print result
 print(T)
